a41a278176583fadf5437cf1ee4b7567f6cd9b7d
Asha <asha.engrbd@gmail.com>
2023-01-12 12:01:02 +0600

    create react project dairy-management-system and install firebase

---
+++ src/__steps.js
@@ -0,0 +1,5 @@
+/**
+ * 1. create react app dairy-management-system
+ * 2. create firebase project > firebase.google.com dairy-management-system 
+ * 3. install firebase using command (npm install firebase)
+ */
