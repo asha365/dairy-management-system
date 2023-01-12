9ebd067d208a1fbfa419e68407eb8b1369e3704a
Asha <asha.engrbd@gmail.com>
2023-01-12 13:37:09 +0600

    complete route setup

--- src/__steps.js
+++ src/__steps.js
@@ -1,4 +1,5 @@
 /**
+ * BASIC PROJECT SETUP
  * 1. create react app dairy-management-system
  * 2. create firebase app > firebase.google.com dairy-management-system 
  * 3. install firebase using command (npm install firebase)
@@ -6,8 +7,14 @@
  * 5. get firebase configuration inside firebase.config.js
  * 6. initialize firebase and here export app
  * 7. install css framework bootstrap and react bootstrap using command (npm install react-bootstrap bootstrap
- * 8. make sure to bootstrap css in the index.js
- * 9.
+ * 8. make sure to import bootstrap css in the index.js
+ * 9. router setup 
  *
  * 
+ */
+
+
+/**
+ * ROUTER SETUP
+ * 1. 
  */
