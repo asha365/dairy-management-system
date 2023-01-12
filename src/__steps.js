4e5c58b4565a726216107b5ac327bd8edba9645a
Asha <asha.engrbd@gmail.com>
2023-01-12 12:21:52 +0600

    install react-router-dom and initialize firebase here export app

--- src/__steps.js
+++ src/__steps.js
@@ -2,4 +2,6 @@
  * 1. create react app dairy-management-system
  * 2. create firebase project > firebase.google.com dairy-management-system 
  * 3. install firebase using command (npm install firebase)
+ * 4. install react-router-dom using command (npm install react-router-dom)
+ * 5. create a folder firebase > firebase.config.js here initialize firebase and here export app
  */
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBKPRk_L_JvhjtpAkBmJYgQrpAU3ycSBv4",
+    authDomain: "dairy-management-system-6fabf.firebaseapp.com",
+    projectId: "dairy-management-system-6fabf",
+    storageBucket: "dairy-management-system-6fabf.appspot.com",
+    messagingSenderId: "266142159421",
+    appId: "1:266142159421:web:49c937a3a16cb95ecc1054"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
