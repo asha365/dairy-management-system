e55e205be1e742dd201516058cb4542772370f3e
Asha <asha.engrbd@gmail.com>
2023-01-12 12:58:51 +0600

    install css framework bootstrap and react bootstrap using command (npm install react-bootstrap bootstrap) and make sure to import bootstrap css in the index.js

--- src/__steps.js
+++ src/__steps.js
@@ -1,7 +1,13 @@
 /**
  * 1. create react app dairy-management-system
- * 2. create firebase project > firebase.google.com dairy-management-system 
+ * 2. create firebase app > firebase.google.com dairy-management-system 
  * 3. install firebase using command (npm install firebase)
  * 4. install react-router-dom using command (npm install react-router-dom)
- * 5. create a folder firebase > firebase.config.js here initialize firebase and here export app
+ * 5. get firebase configuration inside firebase.config.js
+ * 6. initialize firebase and here export app
+ * 7. install css framework bootstrap and react bootstrap using command (npm install react-bootstrap bootstrap
+ * 8. make sure to bootstrap css in the index.js
+ * 9.
+ *
+ * 
  */
