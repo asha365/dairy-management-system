c0b68d649989d3619d4704ea9ad497916f30104e
Asha <asha.engrbd@gmail.com>
2023-01-18 20:13:47 +0600

    inner form field value captures one system using state

--- src/Pages/Others/Profile/Profile.js
+++ src/Pages/Others/Profile/Profile.js
@@ -1,23 +1,38 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { authContext } from '../../../contexts/AuthProvider/AuthProvider';
 
 
 const Profile = () => {
+    const {user} = useContext(authContext);
+    const [name, setName] = useState(user.displayName);
+
+    const handleSubmit = event =>{
+        event.preventDefault();
+        console.log(name);
+    }
+
+    const handleNameChange = event =>{
+        setName(event.target.value)
+    }
     return (
-        <Form>
+        <Form onSubmit={handleSubmit}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-            <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-            </Form.Text>
+            <Form.Control readOnly defaultValue={user?.email} type="email" placeholder="Enter email" />
             </Form.Group>
     
             <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Label>Your Name</Form.Label>
+            <Form.Control onChange={handleNameChange} defaultValue={name} type="text" placeholder="Name" />
             </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Photo URL</Form.Label>
+            <Form.Control defaultValue={user?.photoURL} type="text" placeholder="photo URL" />
+            </Form.Group>
+
             <Form.Group className="mb-3" controlId="formBasicCheckbox">
             <Form.Check type="checkbox" label="Check me out" />
             </Form.Group>
